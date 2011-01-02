93ac49b6b5919d9fb1ef6b878c359aa1fc9d3f3a
Brandon <brandon@melissa-brandon.com>
2011-01-01 22:30:02 -0800

    Paths

--- environment.rb
+++ environment.rb
@@ -6,8 +6,8 @@ end
 configure :production do |config|
   require 'sinatra/cache'
   # NB! you need to set the root of the app first
-  set :root, '/sites/brandon/sleepydriverapp.com/current'
-  set :public, '/sites/brandon/sleepydriverapp.com/current/public'
+  set :root, '/sites/brandon/aisforarray.com/current'
+  set :public, '/sites/brandon/aisforarray.com/current/public'
 
   set :cache_enabled, true  # turn it on
 end
